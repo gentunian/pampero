540b05a5763c80bff1254b0e1349794a71c44118
Sebastian <sebastian.treu@gmail.com>
2013-06-27 18:23:43 -0300

    my_session_("start") is now session_start() equivalent

--- php/poll.php
+++ php/poll.php
@@ -2,7 +2,8 @@
 
 	include_once( __DIR__ . "/../admin/config.php" );
 
-	session_start();
+	//session_start();
+	my_session_( "start" );
 	if ( isset( $_GET['target'] ) && isset( $_SESSION['data'] ) && wrapDomain( $_GET['target'] ) == $_SESSION['target'] ) {
 		echo json_encode( $_SESSION['data'] );
 	} else {
