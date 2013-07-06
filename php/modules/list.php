13d64c35e44301abd3419efa16fb935f218d761d
Sebastian <sebastian.treu@gmail.com>
2013-07-05 21:37:19 -0300

    Class PackageFinder replaces old functionality
    PackageFinder implements getPackages() method in order to retrieve
    package data based on constructor arguments.

--- php/modules/list.php
+++ php/modules/list.php
@@ -1,5 +1,5 @@
-	<?php
-
+<?php
+	
 	//
 	//
 	if ( !defined( 'FROM_PACKAGES' )) {
@@ -9,12 +9,163 @@
 	/**
 	*
 	*/
+	class PackageFinder {
+		private $filter;
+		private $path;
+		private $manifestfilename;
+
+		/**
+		*
+		*/
+		public function __construct( $filter, $path, $manifestfilename ) {
+			$this->filter = $filter;
+			$this->path = $path;
+			$this->manifestfilename = $manifestfilename;
+		}
+
+		/**
+		*
+		*/
+		public function getPackages() {
+			$di = new RecursiveDirectoryIterator( $this->path, FilesystemIterator::SKIP_DOTS );
+			$it = new  RecursiveIteratorIterator( $di, RecursiveIteratorIterator::SELF_FIRST | RecursiveIteratorIterator::LEAVES_ONLY );
+			$it->setMaxDepth( 1 );
+			$data = array();
+			foreach ( $it as $filename => $file ) {
+				if ( $this->isPackageFile( $file )) {
+					$packageData = $this->getPackageDataFromFile( $file );
+					if ( count( $packageData ) > 0 ) {
+						$data = array_merge( $packageData, $data );
+					}
+				}
+			}
+			return $data;
+		}
+
+		/**
+	    * Returns true is $file contains a JSON string called manifest.json.
+	    * False otherwise.
+	    **/
+		private function isPackageFile( $file ) {
+			if ( strcasecmp( $this->manifestfilename, $file->getFilename()) != 0 )
+				return false;
+
+			json_decode( file_get_contents( $file ) );
+			return ( json_last_error() == JSON_ERROR_NONE );
+		}
+
+		/**
+	    * 
+    	**/
+    	private function getPackageDataFromFile( $file ) {
+		    // Retrieve the text from the manifest.json file and
+		    // decode the JSON to an Array Object.
+    		$json = file_get_contents( $file->getPathname() );
+    		$data = json_decode( $json, true );
+
+		    // Get the array that has the installers arrays. $packageData
+		    // now is an array that has installers arrays.
+    		$packageData = $data[ key( $data ) ];
+
+    		return $this->getFilteredData( $packageData );
+    	}
+
+    	/**
+	    * Given a data array, return only those array that its keys
+	    * mathces all keys in the $filter array.
+	    **/
+    	private function getFilteredData( $packageData ) {
+    		$result = array();
+
+    		foreach ($packageData as $key => $installerData) {
+    			if ( $this->filterMatchArray( $installerData, $this->filter['filter'], $this->filter['options']['exact'] ) ) {
+    				$result[] = $installerData;
+    			}
+    		}
+
+    		return $result;
+    	}
+
+	    /**
+	    *
+	    */
+	    private function filterMatchArray( $array, $filterArray, $exact = false ) {
+	    	$matches = 0;
+
+	    	foreach ( array_keys( $filterArray ) as $key ) {
+	    		if ( $this->shouldFilterArray( $this->wrapArray( $array[$key] ), $this->wrapArray( $filterArray[$key] ), $exact ) ) {
+	    			$matches++;
+	    		}
+	    	}
+
+	    	return ( count( $filterArray ) == $matches );
+	    }
+
+	    /**
+	    *
+    	*/
+	    private function wrapArray( $something ) {
+	    	if ( !is_array( $something ) ) {
+	    		return array( $something );
+	    	}
+	    	return $something;
+	    }
+
+    	/**
+    	*
+    	*/
+    	private function shouldFilterArray( $array1, $array2, $exact ) {
+    		foreach( $array1 as $key1 => $value1 ) {
+    			foreach( $array2 as $key2 => $value2 ) {
+    				if ( $this->shouldFilterValue( $value1, $value2, $exact )) {
+    					return true;
+    				}
+    			}
+    		}
+    		return false;
+    	}
+
+    	/**
+    	*
+    	*/
+    	private function shouldFilterValue( $value1, $value2, $exact ) {
+    		if ( $exact ) {
+    			return ( strcasecmp( $value1, $value2 ) == 0 );
+    		} else {
+    			return ( stripos( $value1, $value2 ) !== false );
+    		}
+    	}
+
+    	/**
+    	*
+    	*/
+    	private function valueMatchValue( $value1, $value2, $exact ) {
+    		if ( $exact ) {
+    			return ( strcasecmp( $value1, $value2 ) == 0 );
+    		} else {
+    			if ( is_array($value1) ) {
+    				$value1 = implode(',', $value1);
+    			}
+    			return ( stripos( $value1, $value2 ) !== false );
+    		}
+    	}
+	}
+
+	
+
+	/**
+	*
+	*/
 	function do_list( $args = NULL ) {
 		// Create the filter in order to filter output
 		$filterData = createFilter( $args );
 
-		// Get the packages data
-		$data = getDataFromPath( PACKAGES_DIR, $filterData );
+		// Create a PackageFinder object passing by the filter, the path
+		// where to find packages and the manifest file name it should find.
+		$finder = new PackageFinder( $filterData, PACKAGES_DIR, MANIFEST_FILENAME );
+
+		// Retrieve found packages
+		$data = $finder->getPackages();
 
 		// Return data back
 		return $data;
@@ -73,135 +224,6 @@
 
 	/**
 	*
-	**/
-	function getDataFromPath( $path, $filter = NULL ) {
-		$di = new RecursiveDirectoryIterator( $path, FilesystemIterator::SKIP_DOTS );
-		$it = new  RecursiveIteratorIterator( $di, RecursiveIteratorIterator::SELF_FIRST );
-		$it->setMaxDepth( 1 );
-		$data = array();
-		foreach ( $it as $filename => $file ) {
-			if ( isPackageFile( $file )) {
-				$packageData = getPackageDataFromFile( $file, $filter );
-				if ( count( $packageData ) > 0 ) {
-					$data = array_merge( $packageData, $data );
-				}
-			}
-		}
-		return $data;
-	}
-
-	/**
-	* Returns true is $file contains a JSON string called manifest.json.
-	* False otherwise.
-	**/
-	function isPackageFile( $file ) {
-		if ( strcasecmp( MANIFEST_FILENAME, $file->getFilename()) != 0 )
-			return false;
-
-		json_decode( file_get_contents( $file ) );
-		return ( json_last_error() == JSON_ERROR_NONE );
-	}
-
-
-	/**
-	* 
-	**/
-	function getPackageDataFromFile( $file, $filterData ) {
-		// Retrieve the text from the manifest.json file and
-		// decode the JSON to an Array Object.
-		$json = file_get_contents( $file->getPathname() );
-		$data = json_decode( $json, true );
-
-		// Get the array that has the installers arrays. $packageData
-		// now is an array that has installers arrays.
-		$packageData = $data[ key( $data ) ];
-
-		return getFilteredData( $packageData, $filterData );
-
-	}
-
-	/**
-	* Given a data array, return only those array that its keys
-	* mathces all keys in the $filter array.
-	**/
-	function getFilteredData( $packageData, $filter ) {
-		$result = array();
-
-		foreach ($packageData as $key => $installerData) {
-			if ( filterMatchArray( $installerData, $filter['filter'], $filter['options']['exact'] ) ) {
-				$result[] = $installerData;
-			}
-		}
-
-		return $result;
-	}
-
-	/**
-	*
-	*/
-	function filterMatchArray( $array, $filterArray, $exact = false ) {
-		$matches = 0;
-
-		foreach ( array_keys( $filterArray ) as $key ) {
-			if ( shouldFilterArray( wrapArray( $array[$key] ), wrapArray( $filterArray[$key] ), $exact ) ) {
-				$matches++;
-			}
-		}
-
-		return ( count( $filterArray ) == $matches );
-	}
-
-	/**
-	*
-	*/
-	function wrapArray( $something ) {
-		if ( !is_array( $something ) ) {
-			return array( $something );
-		}
-		return $something;
-	}
-
-	/**
-	*
-	*/
-	function shouldFilterArray( $array1, $array2, $exact ) {
-		foreach( $array1 as $key1 => $value1 ) {
-			foreach( $array2 as $key2 => $value2 ) {
-				if ( shouldFilterValue( $value1, $value2, $exact )) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	/**
-	*
-	*/
-	function shouldFilterValue( $value1, $value2, $exact ) {
-		if ( $exact ) {
-			return ( strcasecmp( $value1, $value2 ) == 0 );
-		} else {
-			return ( stripos( $value1, $value2 ) !== false );
-		}
-	}
-
-	/**
-	*
-	*/
-	function valueMatchValue( $value1, $value2, $exact ) {
-		if ( $exact ) {
-			return ( strcasecmp( $value1, $value2 ) == 0 );
-		} else {
-			if ( is_array($value1) ) {
-				$value1 = implode(',', $value1);
-			}
-			return ( stripos( $value1, $value2 ) !== false );
-		}
-	}
-
-	/**
-	*
 	*/
 	function getStringOutput( $jsonArray, $full ) {
 		$output = sprintf( "\nSe encontraron %d paquete(s):\n", count( $jsonArray ));
@@ -215,7 +237,7 @@
 
 		return $output;
 	}
-
+	
 	/**
 	*
 	*/
@@ -223,7 +245,7 @@
 		$filterData = createFilter( $args );
 
 		$output = "";
-		$outputType = $filterData->output;
+		$outputType = $filterData['options']['output'];
 
 		// Encode JSON if plain json is desired
 		if ( $outputType == "jsonplain" ) {
@@ -247,9 +269,6 @@
 
 			if ( $outputType == "html" ) {
 				$output = "<pre>".$output."</pre>";
-
-			} else if ( $args['output'] == "console" ) {
-
 			}
 		}
 
