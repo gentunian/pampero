6fd292570f2b24bf2402ee158f34f68cad71d2b6
Sebastian <sebastian.treu@gmail.com>
2013-07-10 22:39:35 -0300

    New methods

--- php/sysinfo/SystemInfo.php
+++ php/sysinfo/SystemInfo.php
@@ -4,9 +4,23 @@
 	*
 	*/
 	interface SystemInfo {
+		
+		// Returns the hostname without the domain
 		function getHostname();
+
+		// Returns the domain if any
 		function getDomain();
-		function getOperatingSystem();
+
+		// Returns the operating system name
+		function getOSName();
+
+		//
+		function getOSVersion();
+
+		// Returns the OS architecture. Possible values:
+		//   . "i686" 
+		//   . "x86_64"
+		function getOSArchitecture();
 	}
 
 ?>
