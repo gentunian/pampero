0f3d3099a8698fd8d1d2f2bf232d70e52008835c
Sebastian <sebastian.treu@gmail.com>
2013-07-15 20:17:22 -0300

    Added CredentialsProvider dependency

---
+++ php/sysinfo/WMISystemInfo.php
@@ -0,0 +1,58 @@
+<?php
+	
+class WMISystemInfo
+{
+	private $sysinfo = NULL;
+
+	public function __construct( $hostname )
+	{
+		$user = $credProv->getAdminUser();
+		$password =  $credProv->getAdminPassword();
+		$wmiLocator = new COM("WbemScripting.SWbemLocator");
+		$wmi = $wmiLocator->ConnectServer( $hostname, "root\\CIMV2", $user, $password );
+		$this->sysinfo = array(
+			/*'Win32Product' =>  $wmi->ExecQuery("Select * from Win32_Product"),*/
+			'OperatingSystem' => $wmi->ExecQuery("Select * from Win32_OperatingSystem"),
+			'ComputerSystem' => $wmi->ExecQuery("Select * from Win32_ComputerSystem")
+			/*'Bios' => $wmi->ExecQuery("Select * from Win32_BIOS"),*/
+			/*'Processor' => $wmi->ExecQuery("Select * from Win32_ComputerSystemProcessor"),*/
+			/*'PhysicalMemory' => $wmi->ExecQuery("Select * from Win32_PhysicalMemory"),*/
+			/*'BaseBoard' => $wmi->ExecQuery("Select * from Win32_BaseBoard"),*/
+			/*'LogicalDisk' => $wmi->ExecQuery("Select * from Win32_LogicalDisk")*/
+			);
+	}
+
+	function getHostname()
+	{
+		foreach ( $this->sysinfo['ComputerSystem'] as $wmi_call) 
+			return $wmi_call->Name;
+	}
+
+	function getDomain()
+	{
+		foreach ( $this->sysinfo['ComputerSystem'] as $wmi_call) 
+			return $wmi_call->Domain;
+	}
+
+	function getOSName()
+	{
+		foreach ( $this->sysinfo['OperatingSystem'] as $wmi_call) {
+			$fullname =  explode('|', $wmi_call->Name, 2)[0];
+			return $fullname;
+		}
+	}
+
+	function getOSVersion()
+	{
+		foreach ( $this->sysinfo['OperatingSystem'] as $wmi_call)
+			return $wmi_call->Version;
+	}
+
+	function getOSArchitecture()
+	{
+		foreach ( $this->sysinfo['OperatingSystem'] as $wmi_call)
+			$arch = $wmi_call->OSArchitecture;
+		return preg_replace( "/64.*/", "x86_64", $arch);
+	}
+}
+?>
