ee5907e0b771a1b6e011d99e0f815a71654b5261
Sebastian <sebastian.treu@gmail.com>
2013-07-16 19:47:18 -0300

    Comments added

--- php/sysinfo/WindowsSystemInfo.php
+++ php/sysinfo/WindowsSystemInfo.php
@@ -1,58 +1,87 @@
 <?php
 
+/**
+* WindowsSystemInfo class:
+* ------------------------
+*
+* WindowsSystemInfo class provides information for windows machines. It will try to adapt WMISytemInfo
+* behaviour when possible. A no WMI approach should be made if WMISystemInfo is not available.
+*/
+class WindowsSystemInfo implements SystemInfo
+{
 	// TODO: Provide fallback option if WMI isn't available try guessing with other tools
-	class WindowsSystemInfo {
-		private $sysinfo = NULL;
-		private $wmi = NULL;
 
-		public function __construct( $hostname, $cred ) {
-			try {
-				$this->wmi = new WMISystemInfo( $hostname, $cred );
-			} catch( Exception $e ) {
-				$user = $cred->getAdminUser();
-				$password =  $cred->getAdminPassword();
-				$this->sysinfo['hostname'] = $hostname;
-				$this->sysinfo['domain'] = "Unknown";
-				$this->sysinfo['OSName'] = "Unknown";
-				$this->sysinfo['OSVersion'] = "Unknown";
-				$this->sysinfo['OSArchitecture'] = "Unknown";
-				//echo $e->getMessage();
-			}
-		}
+	private $sysinfo = NULL;
 
-		function getHostname() {
-			if ( $this->wmi == NULL)
-				return $this->sysinfo['hostname'];
+	// Adapt this class to WMISystemInfo when possible.
+	private $wmi = NULL;
 
-			return $this->wmi->getHostname();
-		}
+	/**
+	* Constructor.
+	*
+	* @param hostname The host name from where we want to retrieve info
+	* @param cred Credentials for hostname
+	*/
+	public function __construct( $hostname, $cred )
+	{
+		try {
+			// Try creating a WMISystemInfo object in order to adapt the behaviour.
+			$this->wmi = new WMISystemInfo( $hostname, $cred );
 
-		function getDomain() {
-			if ( $this->wmi == NULL)
-				return $this->sysinfo['domain'];
+		} catch( Exception $e ) {
+			// TODO: If we could not use WMISystemInfo for windows hosts, find out how we could
+			// do this disregarding what OS we are running on...
+			$user = $cred->getAdminUser();
+			$password =  $cred->getAdminPassword();
+			$this->sysinfo['hostname'] = $hostname;
 
-			return $this->wmi->getDomain();
-		}
-
-		function getOSName() {
-			if ( $this->wmi == NULL)
-				return $this->sysinfo['OSName'];
-
-			return $this->wmi->getOSName();
-		}
-
-		function getOSVersion() {
-			if ( $this->wmi == NULL)
-				return $this->sysinfo['OSVersion'];
-
-			return $this->wmi->getOSVersion();
-		}
-
-		function getOSArchitecture() {
-			if ( $this->wmi == NULL)
-				return $this->sysinfo['OSArchitecture'];
-
-			return $this->wmi->getOSArchitecture();
+			// TODO: find out how to determine domain from Unix/Windows enclosed in a Class
+		    // TODO: nmap could do this but will insert an application dependency.
+			$this->sysinfo['domain'] = "Unknown";
+			$this->sysinfo['OSName'] = "Unknown";
+			$this->sysinfo['OSVersion'] = "Unknown";
+			$this->sysinfo['OSArchitecture'] = "Unknown";
 		}
 	}
+
+	function getHostname()
+	{
+		if ( $this->wmi == NULL)
+			return $this->sysinfo['hostname'];
+
+		return $this->wmi->getHostname();
+	}
+
+	function getDomain()
+	{
+		if ( $this->wmi == NULL)
+			return $this->sysinfo['domain'];
+
+		return $this->wmi->getDomain();
+	}
+
+	function getOSName()
+	{
+		if ( $this->wmi == NULL)
+			return $this->sysinfo['OSName'];
+
+		return $this->wmi->getOSName();
+	}
+
+	function getOSVersion()
+	{
+		if ( $this->wmi == NULL)
+			return $this->sysinfo['OSVersion'];
+
+		return $this->wmi->getOSVersion();
+	}
+
+	function getOSArchitecture()
+	{
+		if ( $this->wmi == NULL)
+			return $this->sysinfo['OSArchitecture'];
+
+		return $this->wmi->getOSArchitecture();
+	}
+}
 ?>
