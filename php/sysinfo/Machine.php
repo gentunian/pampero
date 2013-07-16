aea1858b673f8f54dc0dcfd0c862394cc3124a7e
Sebastian <sebastian.treu@gmail.com>
2013-07-16 19:48:06 -0300

    Comments and indentation

--- php/sysinfo/Machine.php
+++ php/sysinfo/Machine.php
@@ -1,29 +1,29 @@
 <?php
 
-	/**
-	*
-	*/
-	class Machine {
-		private $sysinfo = NULL;
-		private $credentials = NULL;
+/**
+*
+*/
+class Machine {
+	private $sysinfo = NULL;
+	private $credentials = NULL;
 
-		public function __construct( $hostname ) {
-			$credProv = new MyCredentialsProvider();
-			// Get credentials from a credentials provider
-			$this->credentials = $credProv->getCredentials( $hostname );
+	public function __construct( $hostname ) {
+		$credProv = new MyCredentialsProvider();
+		// Get credentials from a credentials provider
+		$this->credentials = $credProv->getCredentials( $hostname );
 
-			$systype = Utils::getSystemType( $hostname );
-			$class = $systype."SystemInfo";
-			$this->sysinfo = new $class( $hostname, $this->credentials );
-		}
-	
-		public function getSystemInfo() {
-			return $this->sysinfo;
-		}
-
-		public function getCredentials() {
-			return $this->credentials;
-		}
+		$systype = Utils::getSystemType( $hostname );
+		$class = $systype."SystemInfo";
+		$this->sysinfo = new $class( $hostname, $this->credentials );
 	}
+	
+	public function getSystemInfo() {
+		return $this->sysinfo;
+	}
+
+	public function getCredentials() {
+		return $this->credentials;
+	}
+}
 
 ?>
--- php/sysinfo/SystemInfo.php
+++ php/sysinfo/SystemInfo.php
@@ -1,26 +1,44 @@
 <?php
 
+/**
+* SystemInfo interface:
+* ---------------------
+*
+* SystemInfo interface provides methods for retrieve system information. Implementations should
+* provide, if possible, the information regarding a specific host.
+*
+* NOTE: Implementations that are unable to retrieve some information should return "Unknown" for the
+* required info.
+*/
+interface SystemInfo {
+
 	/**
-	*
+	* Gets the host name from this object is getting system information.
+	* @return The host name this SystemInfo object is providing information for.
 	*/
-	interface SystemInfo {
-		
-		// Returns the hostname without the domain
-		function getHostname();
+	function getHostname();
 
-		// Returns the domain if any
-		function getDomain();
+	/**
+	* @return The domain, if any, where the host named $this->getHostname() belongs.
+	*/
+	function getDomain();
 
-		// Returns the operating system name
-		function getOSName();
+	/**
+	* @return The operating system name 
+	*/
+	function getOSName();
 
-		//
-		function getOSVersion();
+	/**
+	* @return The operating system version
+	*/
+	function getOSVersion();
 
-		// Returns the OS architecture. Possible values:
-		//   . "i686" 
-		//   . "x86_64"
-		function getOSArchitecture();
-	}
+	/**
+	* Implementations should follow a single rule. 32 bits architectures should be named
+	* "i686" and 64 bits "x86_64".
+	* @return The operating system based architecture. Read above
+	*/
+	function getOSArchitecture();
+}
 
 ?>
