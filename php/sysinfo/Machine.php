85bc99a4a889d642715b738f3e12a89ec1e517e4
Sebastian <sebastian.treu@gmail.com>
2013-07-19 20:22:56 -0300

    Arguments check in constructor added.

--- php/sysinfo/Machine.php
+++ php/sysinfo/Machine.php
@@ -8,6 +8,9 @@ class Machine {
 	private $credentials = NULL;
 
 	public function __construct( $hostname ) {
+		if ( $hostname == NULL || !is_string( $hostname ))
+			throw new Exception( "$hostname argument must be a non-null string." );
+
 		$credProv = new MyCredentialsProvider();
 		// Get credentials from a credentials provider
 		$this->credentials = $credProv->getCredentials( $hostname );
