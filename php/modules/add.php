c1f78ccb770fe9ffe7e51790c5bf667fc0bc99e0
Sebastian <sebastian.treu@gmail.com>
2013-09-16 19:29:46 -0300

    Module rewrite.

--- php/modules/add.php
+++ php/modules/add.php
@@ -1,90 +1,63 @@
 <?php
-	/*
-	if ( !isPOSTok() )
-		exit(1);
-	*/
-	define( "PACKAGE_DIR", __DIR__ . "/../../admin/packages" . DIRECTORY_SEPARATOR . $_POST['appKey'] );
-	define( "MANIFEST_FILE", PACKAGE_DIR . DIRECTORY_SEPARATOR . "manifest.json" );
-	// Review: try to find a portable way
-	define( "PACKAGES_SHARE", "\\\\" . gethostname() . "\\packages" );
+	
 
-	/**
-	*
-	*/
-	function main() {
-		$installerData = getArrayFromPOST();
-		if ( file_exists( MANIFEST_FILE ) ) {
-			$packageData = json_decode( file_get_contents( MANIFEST_FILE ), true );
-			array_push( $packageData[ $_POST['appKey'] ], $installerData);
-		} else {
-			$packageData = array( $_POST['appKey'] => array( $installerData ) );
+	function do_add($args)
+	{
+	
+		$opts = new Options(
+			$args,
+			array(),
+			array("name", "description", "os", "arch", "installer", "installerArgs", "version")
+			);
+		$packageName = $opts->getOption("name");
+
+		if (preg_match("/[a-zA-Z ].*/", $packageName) == 1) {
+			
+			$packageData = new PackageData($packageName);
+			$data = $opts->getOptions();
+
+			if (is_array($data['os'])) {
+
+				$count = count($data['os']);
+				for($i = 0; $i < $count; $i++) {
+                    
+                    // Wraps up item data in an array
+					$packageItem = PackageItem::create()->fromArray(
+						array(
+							"name" => $data['name'],
+							"description" => $data['description'],
+							"arch" => $data['arch'][$i],
+							"os" => $data['os'][$i],
+							"version" => $data['version'][$i],
+							"installerArgs" => $data['installerArgs'][$i],
+							"installer" => PACKAGES_SHARE . "\\" . basename($packageData->getPackagePath()) . "\\" . $data['installer']['name'][$i]
+							)
+						);
+
+				    // Adds the package and if successfull, moves the uploaded files
+					if ($packageData->addPackageItem($packageItem)) {
+						$installerPath =  $packageData->getPackagePath() . "/" . $data['installer']['name'][$i];
+						move_uploaded_file($data['installer']['tmp_name'][$i], $installerPath);
+					}
+
+				}
+			} else {
+
+				$packageItem = PackageItem::create()->fromArray($data);
+
+				if ($packageData->addPackageItem($packageItem)) {
+					copy($data['installer'], $packageData->getPackagePath() . "/" . basename($data['installer']));
+				}
+			}
+
+			$packageData->save();
 		}
 
-		if ( !file_exists( PACKAGE_DIR ) )
-			mkdir( PACKAGE_DIR );
-
-		uploadFile( PACKAGE_DIR );
-		file_put_contents( MANIFEST_FILE, json_encode( $packageData, JSON_PRETTY_PRINT ) );
 	}
 
-	/**
-	*
-	*/
-	function uploadFile( $packageDir ) {
-		$temp = $_FILES['appFile']['tmp_name'];
-		$file = "$packageDir" . DIRECTORY_SEPARATOR . $_FILES['appFile']['name'];
-		move_uploaded_file( $temp , $file );
+	function do_add_output($result, $args)
+	{
 	}
 
-	/**
-	*
-	*/
-	function isPOSTok() {
-		return ( isset( $_POST['appName'] ) &&
-			isset( $_POST['appArgs'] ) &&
-			isset( $_POST['appArch'] ) &&
-			isset( $_POST['appOS'] ) &&
-			isset( $_POST['appVersion'] ) &&
-			isset( $_POST['appDescription'] ) &&
-			isset( $_FILES['appFile'] )
-			);
-	}
-
-	/**
-	*
-	*/
-	function getArrayFromPOST() {
-		$output = array(
-			"id" => $_POST['appKey']."-".$_POST['appVersion'].".".$_POST['appArch'],
-			"installer" => PACKAGES_SHARE . "\\" . $_POST['appKey'] . "\\" . $_FILES['appFile']['name'],
-			"installerArgs" => $_POST['appArgs'],
-			"arch" => $_POST['appArch'],
-			"os" => $_POST['appOS'],
-			"version" => $_POST['appVersion'],
-			"name" => $_POST['appName'],
-			"description" => $_POST['appDescription']
-			);
-
-		return $output;
-	}
-
-	/**
-	*
-	*/
-	function getJSONStringFromPOST() {
-		$output = "{\n";
-		//$output .= "\t\"installer\":\"" . $_FILES['appFile']['name'] . "\"\n";
-		$output .= "\t\"installerArgs\":\"" . $_POST['appArgs'] . "\"\n";
-		$output .= "\t\"arch\":\"" . $_POST['appArch'] . "\"\n";
-		$output .= "\t\"os\":\"" . $_POST['appOS'] . "\"\n";
-		$output .= "\t\"version\":\"" . $_POST['appVersion'] . "\"\n";
-		$output .= "\t\"name\":\"" . $_POST['appName'] . "\"\n";
-		$output .= "\t\"description\":\"" . $_POST['appDescription'] . "\"\n";
-		$output .= "\n}";
-
-		return $output;
-	}
-
-
-	main();
+	
 ?>
