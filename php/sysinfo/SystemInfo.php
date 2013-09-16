da56e57e10db7d2db174ca44967fea6089e46cc9
Sebastian <sebastian.treu@gmail.com>
2013-09-16 19:29:28 -0300

    SystemInfo changed to abstract class in order to add behaviour

--- php/sysinfo/SystemInfo.php
+++ php/sysinfo/SystemInfo.php
@@ -1,44 +1,81 @@
 <?php
 
 /**
-* SystemInfo interface:
-* ---------------------
+* SystemInfo abstract class:
+* --------------------------
 *
-* SystemInfo interface provides methods for retrieve system information. Implementations should
+* SystemInfo provides methods for retrieve system information. Implementations should
 * provide, if possible, the information regarding a specific host.
 *
 * NOTE: Implementations that are unable to retrieve some information should return "Unknown" for the
 * required info.
 */
-interface SystemInfo {
+abstract class SystemInfo {
+
+	private $hostname;
 
 	/**
 	* Gets the host name from this object is getting system information.
 	* @return The host name this SystemInfo object is providing information for.
 	*/
-	function getHostname();
+	abstract function getHostname();
 
 	/**
 	* @return The domain, if any, where the host named $this->getHostname() belongs.
 	*/
-	function getDomain();
+	abstract function getDomain();
 
 	/**
 	* @return The operating system name 
 	*/
-	function getOSName();
+	abstract function getOSName();
 
 	/**
 	* @return The operating system version
 	*/
-	function getOSVersion();
+	abstract function getOSVersion();
+
+	/**
+	* @return The IP from this hostname
+	*/
+	function getIP() {
+		return gethostbyname($this->hostname);
+	}
+
+	/**
+	* Constructor.
+	*/
+	public function __construct($hostname) {
+		$this->hostname = $hostname;
+	}
 
 	/**
 	* Implementations should follow a single rule. 32 bits architectures should be named
 	* "i686" and 64 bits "x86_64".
 	* @return The operating system based architecture. Read above
 	*/
-	function getOSArchitecture();
+	abstract function getOSArchitecture();
+
+	public function toString() {
+		$output = "Host name: " . $this->getHostname() . "\n";
+		$output .= "Domain: " . $this->getDomain() . "\n";
+		$output .= "OS name: " . $this->getOSName() . "\n";
+		$output .= "OS version: " . $this->getOSVersion() . "\n";
+		$output .= "OS architecture: " . $this->getOSArchitecture() . "\n";
+		return $output;
+	}
+
+	public function toJSON() {
+		$output = "{";
+		$output .= "hostname: " . $this->getHostname() . ",";
+		$output .= "ip: " . $this->getIP() . ",";
+		$output .= "domain: " . $this->getDomain() . ",";
+		$output .= "osname: " . $this->getOSName() . ",";
+		$output .= "osversion: " . $this->getOSVersion() . ",";
+		$output .= "osarch: " . $this->getOSArchitecture() . ",";
+		$output .= "}";
+		return $output;
+	}
 }
 
 ?>
