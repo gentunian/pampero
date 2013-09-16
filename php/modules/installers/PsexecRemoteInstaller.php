9e53635a10754aa5ecfde39a11d5848110616869
Sebastian <sebastian.treu@gmail.com>
2013-09-16 19:33:04 -0300

    Few changes

--- php/modules/installers/PsexecRemoteInstaller.php
+++ php/modules/installers/PsexecRemoteInstaller.php
@@ -59,7 +59,7 @@ class PsexecRemoteInstaller implements RemoteInstaller {
 
 	public function __destruct() {
 		// Deletes temporal files
-		unlink( $this->scriptFileName );
+		//unlink( $this->scriptFileName );
 		unlink( $this->stdoutFileName );
 		unlink( $this->stderrFileName );
 	}
@@ -80,13 +80,17 @@ class PsexecRemoteInstaller implements RemoteInstaller {
 		$wscript = "var wsh = new ActiveXObject( 'WScript.Shell' );\n";
 		//$wscript .= "var fso = new ActiveXObject( 'Scripting.FileSystemObject' );\n\n";
 		$wscript .= "var psexecArgs = WScript.Arguments.Named;\n";
-		$wscript .= "var commandArgs = WScript.Arguments.Unnamed;\n\n";
+//		$wscript .= "var commandArgs = WScript.Arguments.Unnamed;\n\n";
 		$wscript .= "var cmd = 'cmd.exe /C psexec \\\\\\\\'+psexecArgs.Item('machine')+' -n 60 -u '+psexecArgs.Item('user')+' -p '+psexecArgs.Item('password')+' cmd.exe /C \"';\n";
-		$wscript .= "for (var i = 0; i < commandArgs.length; i++) {\n";
-		$wscript .= "\tvar arg = commandArgs.Item( i );\n";
-		$wscript .= "\tif ( arg.indexOf( \" \" ) >= 0 ) {\n";
-		$wscript .= "\t\targ = '\"'+arg+'\"';\n\t}\n";
-		$wscript .= "\tcmd += ' '+arg+' ';\n}\n";
+		$wscript .= "for (var i = 0; i < WScript.Arguments.length; i++) {\n";
+		$wscript .= "\tvar arg = WScript.Arguments.Item( i );\n";
+		$wscript .= "\tif (arg.indexOf('/machine') != 0 && arg.indexOf('/user') != 0 && arg.indexOf('/password') != 0 ) {\n";
+		$wscript .= "\t\tif ( arg.indexOf( \" \" ) >= 0 ) {\n";
+		$wscript .= "\t\t\targ = '\"'+arg+'\"';\n";
+		$wscript .= "\t\t}\n";
+		$wscript .= "\t\tcmd += ' '+arg+' ';\n";
+		$wscript .= "\t}\n";
+		$wscript .= "}\n";
 		$wscript .= "cmd += '\"';\n\n";
 		$wscript .= "var env = wsh.Environment(\"PROCESS\");\n";
 		$wscript .= "env(\"SEE_MASK_NOZONECHECKS\") = 1;\n";
@@ -100,19 +104,21 @@ class PsexecRemoteInstaller implements RemoteInstaller {
 	}
 
 
-
 	public function install( $command, $args, $machine, $credentials) {
 		$psexecOptions = "/user:".$credentials->getAdminUser();
 		$psexecOptions .= " /password:".$credentials->getAdminPassword();
 		$psexecOptions .= " /machine:".$machine;
 
-		$string = "cscript //E:JScript ".$this->scriptFileName." $psexecOptions \"$command\" $args";
-		
+		if (strpos($command, " ") > 0){
+			$command = "\"$command\"";
+		}
+		$string = "cscript //E:JScript ".$this->scriptFileName." ${psexecOptions} ${command} ${args}";
+		file_put_contents("bah",$string);
 		set_time_limit(120);
 		$process = popen( $string, "r" );
 		$exitCode = pclose( $process );
 
-		$exitString = iconv('CP1252', 'UTF-8//IGNORE', preg_replace('/\n/m', '', shell_exec("net helpmsg $exitCode")));
+		$exitString = iconv('CP1252', 'UTF-8//IGNORE', preg_replace('/\n/m', '', shell_exec("net helpmsg ${exitCode}")));
 		$stdout = iconv('CP1252', 'UTF-8', file_get_contents( $this->stdoutFileName ));
 		$stderr = iconv('CP1252', 'UTF-8//IGNORE', file_get_contents( $this->stderrFileName ));
 
